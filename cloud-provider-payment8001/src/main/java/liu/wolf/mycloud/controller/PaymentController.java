b579f77ec79d40c6eb841050304a00ef31632e1f
Wolf-Liu <iam.hugeman4@qq.com>
2020-10-19 00:15:26 +0800

    - 新增 .gitignore ； - 结束 eureka 学习。

--- cloud-provider-payment8001/src/main/java/liu/wolf/mycloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/liu/wolf/mycloud/controller/PaymentController.java
@@ -5,7 +5,12 @@ import liu.wolf.mycloud.entity.Payment;
 import liu.wolf.mycloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * Demo PaymentController
@@ -19,6 +24,9 @@ import org.springframework.web.bind.annotation.*;
 public class PaymentController {
     @Autowired
     private PaymentService paymentService;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @PostMapping
     public CommonResult<Integer> create(@RequestBody Payment payment) {
@@ -36,5 +44,19 @@ public class PaymentController {
             return new CommonResult<>(200, "成功 (8001)", payment);
         else
             return new CommonResult<>(404, "没有找到订单 (8001)");
+    }
+
+    @GetMapping("discovery")
+    public DiscoveryClient getDiscoveryClient() {
+        List<String> services = discoveryClient.getServices();
+        for (String service : services) {
+            log.info(service);
+        }
+
+        List<ServiceInstance> instances = discoveryClient.getInstances("cloud-payment-service");
+        for (ServiceInstance instance : instances) {
+            log.info(instance.getInstanceId() + "\t|" + instance.getHost() + "\t|" + instance.getPort() + "\t|" + instance.getUri());
+        }
+        return discoveryClient;
     }
 }
