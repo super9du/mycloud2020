b5475bbc56d5540bae1bda2cbc1fce670d6080b9
Wolf-Liu <iam.hugeman4@qq.com>
2020-11-22 14:19:07 +0800

    结束 nacos 学习

--- cloudalibaba-provider-payment9002/src/main/java/liu/wolf/mycloud/controller/NacosPaymentController.java
+++ cloudalibaba-provider-payment9002/src/main/java/liu/wolf/mycloud/controller/NacosPaymentController.java
@@ -1,6 +1,7 @@
 package liu.wolf.mycloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,14 +15,24 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("payment")
+@RefreshScope
 public class NacosPaymentController {
     @Value("${spring.application.name}")
     private String serverName;
+
     @Value("${server.port}")
     private String port;
+
+    @Value(("${config.info}"))
+    private String configInfo;
 
     @GetMapping("{id}")
     public String get(@PathVariable("id") Long id) {
         return "Server Name: " + serverName + "<br>From Port: " + port + "<br>ID: " + id;
+    }
+
+    @GetMapping("config")
+    public String getConfig() {
+        return "Server Name: " + serverName + "<br>From Port: " + port + "<br>configInfo: " + configInfo;
     }
 }
